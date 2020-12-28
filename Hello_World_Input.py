aa80de561d919a83440f4bff6ee736f7ec61b3e6
Jlinthon <75713398+jlinthon@users.noreply.github.com>
2020-12-28 08:21:12 -0500

    Add files via upload

---
+++ Hello_World_Input.py
@@ -0,0 +1,13 @@
+#This code receives input from the User# 
+import sys
+import os
+print("Hello World")
+# print("STRING") #
+print("What is your Name and What is your Age?")
+# print("STRING") #
+my_Name=input("What is your Name?: ")
+# VARIABLE_1 = input("STRING") #
+my_Age=input("What is your Age?: ")
+# VARIABLE_2 = input("STRING") #
+print('Hi, your name is ' + my_Name + ' and you are ' + my_Age + ' Years old!!')
+# print("STRING" + VARIABLE_1 + "STRING" + VARIABLE_2 + "STRING")
