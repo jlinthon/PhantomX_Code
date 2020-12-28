92867b723b67f9f63fe6ac9a639273d90a81985e
Jlinthon <75713398+jlinthon@users.noreply.github.com>
2020-12-28 08:11:13 -0500

    Add files via upload

---
+++ Hello_World_Input.py
@@ -0,0 +1,7 @@
+import sys
+import os
+print("Hello World")
+print("What is your Name and What is your Age?")
+my_Name=input("What is your Name?: ")
+my_Age=input("What is your Age?: ")
+print('Hi, your name is ' + my_Name + ' and you are ' + my_Age + ' Years old!!')
