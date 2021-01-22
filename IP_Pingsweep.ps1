9d4fa2d97e3307b1adcdc24be5849e479c7fe7bd
Jlinthon <75713398+jlinthon@users.noreply.github.com>
2021-01-21 22:43:51 -0500

    Update IP_Pingsweep.ps1

--- IP_Pingsweep.ps1
+++ IP_Pingsweep.ps1
@@ -2,12 +2,12 @@ $Output= @()
 $names = Get-content "C:\Users\Administrator\Desktop\IP_List.txt"
 foreach ($name in $names){
   if (Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
-   $Output+= "$name,up"
-   Write-Host "$Name,up"
+   $Output+= "$name is up"
+   Write-Host "$Name is up" -ForegroundColor Green
   }
   else{
-    $Output+= "$name,down"
-    Write-Host "$Name,down"
+    $Output+= "$name is down"
+    Write-Host "$Name is down" -ForegroundColor Red
   }
 }
 $Output | Out-file "C:\Users\Administrator\Desktop\result.txt"
